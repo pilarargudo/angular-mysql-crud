3167de3ac3ce6836bd625b771a96a6ac8db64a60
Pilar Argudo <pargudo@disid.com>
2019-05-20 18:47:43 +0200

    Added database schema

---
+++ database/database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ng_games_db;
+
+USE ng_games_db;
+
+CREATE TABLE games(
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(180),
+  description VARCHAR(255),
+  image VARCHAR(200),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE games;
