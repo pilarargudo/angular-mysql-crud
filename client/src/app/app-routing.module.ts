a865d4213aa8708d9dc45769924bc3dcdfddceae
Pilar Argudo <pargudo@disid.com>
2019-05-21 18:48:05 +0200

    Added routes path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameListComponent } from './components/game-list/game-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/games', pathMatch: 'full'},
+  {path: 'games', component: GameListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
