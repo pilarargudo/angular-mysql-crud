f8abd7c3b0b864a5a1ce32595f6a8a7af6d574bf
Pilar Argudo <pargudo@disid.com>
2019-05-21 18:31:07 +0200

    Added angular client with components

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    GameFormComponent,
+    GameListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
