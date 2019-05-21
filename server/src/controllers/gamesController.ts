0ef60aa7a5feae1302782a1ec8f642ba7f7b967c
Pilar Argudo <pargudo@disid.com>
2019-05-21 18:18:24 +0200

    Added list, getone, create, delete and update api

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -8,11 +8,19 @@ class GamesController {
     const games = await pool.query('SELECT * from games');
     res.json(games);
   }
-  public getOne (req: Request, res: Response) {
-    //pool.query('DESCRIBE games');
-    res.json('este es el juego ' + req.params.id );
+  public async getOne (req: Request, res: Response): Promise<void> {
+    //res.json('este es el juego ' + req.params.id );
+    const { id } = req.params;
+    const game = await pool.query('SELECT * FROM games WHERE id = ?', [id] );
+    console.log(game);
+    if(game.length > 0) {
+      //res.json({text: 'Juego encontrado'});
+      // devuelve el objeto y no el array
+      res.json(game[0]);
+    }
+    res.status(404).json({text: 'Lo siento, juego no encontrado'});
   }
-  public async create (req: Request, res: Response): Promise<void> {
+  public async create (req: Request, res: Response): Promise<any> {
     // datos  que recibimos
     console.log(req.body);
     //enviamos a la base de datos, como puede tomar tiempo async await
@@ -20,12 +28,18 @@ class GamesController {
     res.json({message: 'Juego guardado'});
   }
 
-  public delete (req: Request, res: Response) {
-    res.json({text: 'eliminando juego ' + req.params.id});
+  public async delete (req: Request, res: Response): Promise<void> {
+    //res.json({text: 'eliminando juego ' + req.params.id});
+    const { id } = req.params;
+    await pool.query('DELETE FROM games WHERE id = ?', [id] );
+    res.json({message: 'eliminado el juego ' + id});
   }
 
-  public update (req: Request, res: Response) {
-    res.json({text: 'actualizando juego ' + req.params.id});
+  public async update (req: Request, res: Response): Promise<void>  {
+    //res.json({text: 'actualizando juego ' + req.params.id});
+    const { id } = req.params;
+    await pool.query('UPDATE games set ? WHERE id = ?', [req.body, id]);
+    res.json({message: 'actualizado el juego ' + id});
   }
 }
 
